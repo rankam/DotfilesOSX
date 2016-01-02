40bc50eede58ef549cf3a4247b0adc7534c5b535
Mark Gannaway <mark@ganners.co.uk>
2016-01-02 12:11:33 +0000

    Changed ctags type

--- scripts/install.sh
+++ scripts/install.sh
@@ -28,7 +28,7 @@ which -s tmux || brew install tmux
 which -s htop || brew install htop
 which -s wget || brew install wget
 which -s wemux || brew install wemux
-which -s ctags || brew install ctags
+which -s ctags || brew install ctags-exuberant
 which -s ssh-copy-id || brew install ssh-copy-id
 
 # Install grep (might be required for powerline on tmux)
